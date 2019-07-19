a592c4353dc4d0acda4f9e22a807231734aa9b6e
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-07-19 17:52:30 -0400

    Fixing compress string

--- string_compression/string_compression.go
+++ string_compression/string_compression.go
@@ -1,5 +1,38 @@
 package compression
 
+import (
+	"strconv"
+	"strings"
+)
+
 func compressString(input string) string {
-	return ""
+	var sb strings.Builder
+
+	var b rune
+	var bCount int
+	for i, c := range input {
+		if i > 0 {
+			if c == b {
+				bCount++
+				continue
+			} else {
+				sb.WriteRune(b)
+				sb.WriteString(strconv.Itoa(bCount))
+			}
+		}
+
+		b = c
+		bCount = 1
+	}
+
+	sb.WriteRune(b)
+	sb.WriteString(strconv.Itoa(bCount))
+
+	output := sb.String()
+
+	if len(input) <= len(output) {
+		return input
+	}
+
+	return output
 }
